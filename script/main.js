a19b17c80435f9f3a56cb6f76c6f6bbf3333dcfa
Rohit Saraff <rohitsaraff@gmail.com>
2022-04-21 23:29:04 +0545

    Test animation hold until page load

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', (e) => {
   console.log('Loaded');
   document.querySelector("body").classList.remove("preload");
+  slideShow();
 });
 
 
@@ -9,14 +10,17 @@ slideImages.forEach((img, index) => {
   if (index > 0) img.style.display = "none";
 });
 
-setInterval(function () {
-  const img = slideImages.shift();
-  img.style.zIndex = 0
-  slideImages.push(img);
-  slideImages[0].style.zIndex = 10;
-  slideImages[0].style.display = "block";
-  fadeOut(img);
-}, 5000);
+
+function slideShow() {
+  setInterval(function () {
+    const img = slideImages.shift();
+    img.style.zIndex = 0
+    slideImages.push(img);
+    slideImages[0].style.zIndex = 10;
+    slideImages[0].style.display = "block";
+    fadeOut(img);
+  }, 5000);
+}
 
 function fadeOut(img) {
   setTimeout(function () { img.style.display = "none" }, 3000);
