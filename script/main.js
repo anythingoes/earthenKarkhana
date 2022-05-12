1384b4461f0d11de1aa4f7ba8202fd339e18e666
rohit saraff <rohitsaraff@gmail.com>
2022-05-13 02:59:34 +0545

    Unable to fix mobile-animation issue

--- script/main.js
+++ script/main.js
@@ -1,53 +1,39 @@
 "use strict";
 
-let navbar = document.querySelector('header');
+const slideImages = Array.from(document.querySelectorAll(".image-slider__img"));
 
-// Sticky navbar
-window.addEventListener("scroll", e => {
-  let scrollPos = window.scrollY;
-  console.log(`scrollPos: ${scrollPos}`);
-
-  if (scrollPos > 0) {
-    navbar.classList.add("header--sticky");
-  } else {
-    navbar.classList.remove("header--sticky");
-  }
-});
-
-
-
-// Wait for all images to load before starting slideshow 
+// Wait for page to load before starting slideshow
 window.addEventListener('load', (e) => {
   document.querySelector("body").classList.remove("preload");
 
+  // slideshow should only run on index page 
   if (document.querySelectorAll(".image-slider__img").length == 0) return;
 
   // Static image not required to be visible once slide images are loaded in
   setTimeout(() => document.querySelector(".no-slide").style.opacity = 0, 8000);
+
   slideShow();
 });
 
 
 function slideShow() {
-  console.log('start animation');
-  const slideImages = Array.from(document.querySelectorAll(".image-slider__img"));
-
   slideImages.forEach((img, index) => {
     img.style.display = "none";
   });
-
-  setInterval(function () {
-    const img = slideImages.shift();
-    img.style.zIndex = 0
-    slideImages.push(img);
-    slideImages[0].style.zIndex = 10;
-    slideImages[0].style.display = "block";
-
-    fadeOut(img);
-  }, 5000);
-  // window.requestAnimationFrame(slideShow);
+  let animationInterval = setInterval(startSlideShow, 5000);
 }
 
-function fadeOut(img) {
+function startSlideShow() {
+  console.log('starting slideshow');
+  const img = slideImages.shift();
+  img.style.zIndex = 0
+  slideImages.push(img);
+  slideImages[0].style.zIndex = 10;
+  slideImages[0].style.display = "block";
+
+  removePreviousImage(img);
+}
+
+function removePreviousImage(img) {
   setTimeout(function () { img.style.display = "none" }, 3000);
 }
